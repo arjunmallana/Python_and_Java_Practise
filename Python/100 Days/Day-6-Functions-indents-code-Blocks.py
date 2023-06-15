7a4e5ece68092184e1ea5fbd6dd7773233390aef
Sidilesh Mallanna <arjunmallana@gmail.com>
2023-06-15 06:41:53 +0530

    Update Day-6-Functions-indents-code-Blocks.py

---
+++ Python/100 Days/Day-6-Functions-indents-code-Blocks.py
@@ -0,0 +1,16 @@
+# Functions and Indents
+
+# Functions 
+# Functions are basically classes , where there are in-built functions
+# such as random() etc and we can create our own functions 
+def hello_world():
+    print("Hello")
+    print("World")
+a=int(input("Int:"))
+b=float(input("Float:"))
+c=bool(input("Boolean:"))
+d=char(input("Char:"))
+print(f"{type(a)} {type(b)} {type(c)} {type(d)}")
+hello_world()
+
+# While loop 
